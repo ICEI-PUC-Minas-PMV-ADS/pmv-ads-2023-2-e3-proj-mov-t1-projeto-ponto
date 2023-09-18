6fe03b9727308c55711d13620a88a16c34ede72e
Felipe Ferreira <114882542+Felipeadf@users.noreply.github.com>
2023-09-18 20:14:18 -0300

    Ajuste Página de Histórico

--- src/projeto-ponto/App.js
+++ src/projeto-ponto/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Perfil Funcionário</Text>
+      <Text>Histórico de Ponto</Text>
       <StatusBar style="auto" />
     </View>
   );
