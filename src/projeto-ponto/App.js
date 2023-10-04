3fc1846726ea74886c3d332e512a8d5380988eee
Vinicius Carneiro Gonçalves <105397334+viniciuscg@users.noreply.github.com>
2023-10-03 21:05:56 -0300

    adc pages

--- src/projeto-ponto/App.js
+++ src/projeto-ponto/App.js
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import WorkerProfile from './views/workerProfile/index'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Histórico de Ponto</Text>
-      <StatusBar style="auto" />
+    <View>
+      <WorkerProfile/>
     </View>
   );
 }
@@ -13,7 +12,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
--- src/projeto-ponto/views/workerProfile/index.js
+++ src/projeto-ponto/views/workerProfile/index.js
@@ -1,19 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Histórico de Ponto</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Text style={ styles.container }>
+      
+    </Text>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
