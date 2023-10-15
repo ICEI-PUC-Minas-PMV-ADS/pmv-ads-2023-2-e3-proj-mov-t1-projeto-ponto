dca3030f0990c716c746a26d8642c9d163ef5eaf
Felipe Ferreira <114882542+Felipeadf@users.noreply.github.com>
2023-10-15 15:29:58 -0300

    Histórico de Ponto

--- src/frontend/views/PointHistory/PointHistory.jsx
+++ src/frontend/views/PointHistory/PointHistory.jsx
@@ -1,10 +1,23 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import { styles } from "./styles";
 
 export const PointHistory = () => {
   return (
-    <View style={styles.container}>
-      <Text>Histórico de Ponto</Text>
+    <View style={styles.title}>
+      <Text style={styles.text}>Histórico de Ponto</Text>
+      <Text>Nome do Colaborador</Text>
+      <View style={styles.dateInputContainer}>
+        <TextInput
+        style={styles.dateInput}
+        placeholder= "Data Início"
+        />
+        <Text style={styles.dateInput}>à</Text>
+        <TextInput
+        style={styles.dateInput}
+        placeholder= "Data Fim"
+        />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
--- src/frontend/views/PointHistory/styles.js
+++ src/frontend/views/PointHistory/styles.js
@@ -1,8 +1,26 @@
+import { StyleSheet, Text, View } from "react-native";
+
 export const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
+  title: {
+    backgroundColor: "#ffffff",
+    alignItems: "flex-start",
     justifyContent: "center",
+    paddingTop: 50,
+    paddingLeft: 15,
+    
   },
+  text: {
+    fontSize: 30,
+  },
+  dateInputContainer: {
+    padding: 10,
+    flexDirection: "row", 
+    alignItems: "center", 
+  },
+  dateInput: {
+    /* flex: 1, */
+    margin: 5, 
+   
+  },
+
 });
