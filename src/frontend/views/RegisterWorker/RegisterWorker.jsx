8db1de7c7ece8860463bd9421e1dc876323e217c
Leonardo Igor <115428616+leosportt@users.noreply.github.com>
2023-10-15 21:17:12 -0300

    Update RegisterWorker.jsx

--- src/frontend/views/RegisterWorker/RegisterWorker.jsx
+++ src/frontend/views/RegisterWorker/RegisterWorker.jsx
@@ -1,22 +1,29 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { UserServices } from "../../services/user/userServices";
+import { SafeAreaView, Text, View, Image } from "react-native";
+import { Button, Input } from "../../components";
+import { styles } from "./styles";
+import { COLORS } from "../../constants";
 
 export const RegisterWorker = () => {
-  const [msgErrorRegister, setMsgErrorRegister] = useState("");
-
-  const registerUser = async (e) => {
-    try {
-      e.preventDefault();
-      await UserServices.registerUser();
-    } catch (error) {
-      setMsgErrorRegister(error.message);
-    }
-  };
   return (
-    <View style={styles.container}>
-      <Text>Histórico de Ponto</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.logoContainer}>
+        <Image style={styles.logo} source={require("../../assets/icon.png")} />
+      </View>
+      <View style={{ marginBottom: 50 }}>
+        <Text style={styles.title}>Registrar (admin)</Text>
+      </View>
+      <Input label="Nome completo" style={{ marginBottom: 25 }} name />
+      <Input label="Email" style={{ marginBottom: 25 }} email/>
+      <Input label="Cargo" style={{ marginBottom: 25 }} name/>
+      <Input label="Senha" style={{ marginBottom: 25 }} password />
+      <Input label="Repita sua senha" style={{ marginBottom: 25 }} password />
+      <View style={{ width: "100%" }}>
+        <Button
+          title="Registrar"
+          onPress={() => console.log("teste")}
+          style={{ width: 300 }}
+        />
+      </View>
+    </SafeAreaView>
   );
 };
