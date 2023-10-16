b1179e3878bab76d247b4f45bb798625cab28f54
Leonardo Igor <115428616+leosportt@users.noreply.github.com>
2023-10-15 21:19:30 -0300

    Update RegisterWorker

--- src/frontend/App.jsx
+++ src/frontend/App.jsx
@@ -1,12 +1,14 @@
 import { StatusBar, View, Text } from "react-native";
 import { Login, WorkerProfile, PointHistory } from "../frontend/views";
 import { PasswordRegister } from "./views/PasswordRegister/PasswordRegister";
+import { RegisterWorker } from "./views/RegisterWorker/RegisterWorker";
 
 export default function App() {
   return (
     <View>
       {/* <Text>Teste do app</Text> */}
       {/* <WorkerProfile /> */}
+      {/* <RegisterWorker /> */}
       <PasswordRegister />
     </View>
   );
