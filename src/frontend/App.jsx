2bb72c1bd0709b3f96f50c50656e4946b4dbab4e
RussoLucas <lukasrusso22@gmail.com>
2023-11-27 23:08:52 -0300

    adding auth provider logic

--- src/frontend/App.jsx
+++ src/frontend/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useMemo, useReducer } from "react";
 import {
   Login,
   WorkerProfile,
@@ -8,21 +8,51 @@ import {
 } from "../frontend/views";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { authReducer } from "./reducers";
+import { AuthContext } from "./context";
+
+const DEFAULT_AUTH_OBJECT = {
+  isLoading: true,
+  isSignout: false,
+  userToken: null,
+};
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [isAdmin, setIsAdmin] = useState(false);
+  const [authState, dispatch] = useReducer(authReducer, DEFAULT_AUTH_OBJECT);
+
+  const authContext = useMemo(() => ({
+    signIn: async (data) => {
+      dispatch({ type: "SIGN_IN", token: "dummy-auth-token" });
+    },
+    signOut: () => dispatch({ type: "SIGN_OUT" }),
+    signUp: async (data) => {
+      dispatch({ type: "SIGN_IN", token: "dummy-auth-token" });
+    },
+  }));
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName={isAdmin ? "Register" : "WorkerProfile"}>
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="PasswordRegister" component={PasswordRegister} />
-        <Stack.Screen name="PointHistory" component={PointHistory} />
-        <Stack.Screen name="WorkerProfile" component={WorkerProfile} />
-        <Stack.Screen name="Register" component={Register} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <AuthContext.Provider value={authContext}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          {authState.userToken === null ? (
+            <>
+              <Stack.Screen name="Login" component={Login} />
+              <Stack.Screen name="Register" component={Register} />
+              <Stack.Screen
+                name="PasswordRegister"
+                component={PasswordRegister}
+              />
+            </>
+          ) : (
+            <>
+              <Stack.Screen name="PointHistory" component={PointHistory} />
+              <Stack.Screen name="WorkerProfile" component={WorkerProfile} />
+            </>
+          )}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </AuthContext.Provider>
   );
 }
